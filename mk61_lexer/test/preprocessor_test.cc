34c594dad5fd66a1596985887ea53039a2aab15c
Denis Kiselev <denis.kiselyov@gmail.com>
2024-07-18 15:33:03 +0300

    Add tests with more sense (#11)

--- mk61_lexer/test/preprocessor_test.cc
+++ mk61_lexer/test/preprocessor_test.cc
@@ -1,6 +1,17 @@
 #include <gtest/gtest.h>
 
-TEST(General, Workability) {
-	EXPECT_STRNE("Hello", "World");
-	EXPECT_EQ(25 * 12, 300);
-}
+#include "Preprocessor.h"
+
+namespace lexer_common_suite {
+	TEST(General, ReturnsInputForPerfectInput) {
+		Preprocessor *subject = new Preprocessor();
+		std::string input = "Hello, World!";
+		EXPECT_EQ(input, subject->call(input));
+	}
+
+	TEST(General, ReturnsInputForEmptyInput) {
+		Preprocessor *subject = new Preprocessor();
+		std::string input = "";
+		EXPECT_EQ(input, subject->call(input));
+	}
+}  // namespace lexer_common_suite
