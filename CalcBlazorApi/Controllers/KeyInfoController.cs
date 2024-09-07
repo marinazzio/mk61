218785419291e0824bb14e713423e82c007b277a
Denis Kiselyov <denis.kiselyov@gmail.com>
2024-09-07 20:21:12 +0700

    Implement interface from web to the backend

---
+++ CalcBlazorApi/Controllers/KeyInfoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CalcBlazorCore.Models;
+
+namespace CalcBlazorApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class KeyInfoController : Controller
+{
+    [HttpGet("{keyNumber}")]
+    public ActionResult<KeyInfo> GetKeyInfo(string keyNumber)
+    {
+        var keyInfo = new KeyInfo
+        {
+            Code = "53",
+            IsFPressed = false,
+            IsKPressed = false
+        };
+
+        return Ok(keyInfo);
+    }
+}
---
+++ CalcBlazorCore/Models/KeyInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace CalcBlazorCore.Models;
+
+public class KeyInfo
+{
+    public required string Code { get; set; }
+    public bool IsFPressed { get; set; }
+    public bool IsKPressed { get; set; }
+
+    public string? PreparedFunction { get; set; }
+}
