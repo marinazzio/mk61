7f8be11c858c2d6bdf62a6050a49fe23c128901c
Denis Kiselyov <denis.kiselyov@gmail.com>
2024-09-17 11:35:00 +0700

    Rollback to clr linking

--- keyboard/src/Keyboard.h
+++ keyboard/src/Keyboard.h
@@ -1,24 +1,18 @@
 #pragma once
 
-#ifdef KEYBOARD_EXPORTS
-#define KEYBOARD_API __declspec(dllexport)
-#else
-#define KEYBOARD_API __declspec(dllimport)
-#endif
-
 #include <cstdint>
 
 public ref class Keyboard
 {
 public:
-    KEYBOARD_API Keyboard();
-    KEYBOARD_API ~Keyboard();
+    Keyboard();
+    ~Keyboard();
 
-	property bool IsFPressed;
+    property bool IsFPressed;
     property bool IsKPressed;
 
-	property uint8_t CmdBuffer;
+    property uint8_t CmdBuffer;
 
-    KEYBOARD_API int GetCodeByKeyPosition(int keyPosition);
-    KEYBOARD_API void PressByPosition(int keyPosition);
+    int GetCodeByKeyPosition(int keyPosition);
+    void PressByPosition(int keyPosition);
 };
