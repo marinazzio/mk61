1279c989c02eb12f0f1540df5986644cb5c5b387
Denis Kiselyov <denis.kiselyov@gmail.com>
2024-09-17 11:25:06 +0700

    Export lib interface

--- keyboard/src/Keyboard.h
+++ keyboard/src/Keyboard.h
@@ -1,17 +1,24 @@
 #pragma once
+
+#ifdef KEYBOARD_EXPORTS
+#define KEYBOARD_API __declspec(dllexport)
+#else
+#define KEYBOARD_API __declspec(dllimport)
+#endif
+
 #include <cstdint>
 
 public ref class Keyboard
 {
 public:
-    Keyboard();
-    virtual ~Keyboard();
+    KEYBOARD_API Keyboard();
+    KEYBOARD_API ~Keyboard();
 
 	property bool IsFPressed;
     property bool IsKPressed;
 
 	property uint8_t CmdBuffer;
 
-    int GetCodeByKeyPosition(int keyPosition);
-	void PressByPosition(int keyPosition);
+    KEYBOARD_API int GetCodeByKeyPosition(int keyPosition);
+    KEYBOARD_API void PressByPosition(int keyPosition);
 };
